b46124985191a7b0ed57359ecadc308b717f326c
MarieAmp <marymary09@hotmail.com>
2020-07-30 16:56:36 +0200

    seeds quizz ok

--- bin/seeds/quizz.seeds.js
+++ bin/seeds/quizz.seeds.js
@@ -1,16 +1,19 @@
 var axios = require("axios");
+require("dotenv").config(); 
+require("../../config/dbConnection");
+const QuizzModel= require('../../models/Quizz')
 
 const quizzUrl = [
   "https://www.kiwime.com/oqdb/files/2164852257/OpenQuizzDB_164/openquizzdb_164.json",
-    "https://www.kiwime.com/oqdb/files/2165472333/OpenQuizzDB_165/openquizzdb_165.json",
-    "https://www.kiwime.com/oqdb/files/2167325638/OpenQuizzDB_167/openquizzdb_167.json",
-    "https://www.kiwime.com/oqdb/files/2168235464/OpenQuizzDB_168/openquizzdb_168.json",
-    "https://www.kiwime.com/oqdb/files/2174524525/OpenQuizzDB_174/openquizzdb_174.json",
-    "https://www.kiwime.com/oqdb/files/3220495553/OpenQuizzDB_220/openquizzdb_220.json",
-    "https://www.kiwime.com/oqdb/files/3219637788/OpenQuizzDB_219/openquizzdb_219.json",
-    'https://www.kiwime.com/oqdb/files/3224652244/OpenQuizzDB_224/openquizzdb_224.json',
-    "https://www.kiwime.com/oqdb/files/3233548326/OpenQuizzDB_233/openquizzdb_233.json",
-    "https://www.kiwime.com/oqdb/files/2166587884/OpenQuizzDB_166/openquizzdb_166.json",
+  "https://www.kiwime.com/oqdb/files/2165472333/OpenQuizzDB_165/openquizzdb_165.json",
+  "https://www.kiwime.com/oqdb/files/2167325638/OpenQuizzDB_167/openquizzdb_167.json",
+  "https://www.kiwime.com/oqdb/files/2168235464/OpenQuizzDB_168/openquizzdb_168.json",
+  "https://www.kiwime.com/oqdb/files/2174524525/OpenQuizzDB_174/openquizzdb_174.json",
+  "https://www.kiwime.com/oqdb/files/3220495553/OpenQuizzDB_220/openquizzdb_220.json",
+  "https://www.kiwime.com/oqdb/files/3219637788/OpenQuizzDB_219/openquizzdb_219.json",
+  'https://www.kiwime.com/oqdb/files/3224652244/OpenQuizzDB_224/openquizzdb_224.json',
+  "https://www.kiwime.com/oqdb/files/3233548326/OpenQuizzDB_233/openquizzdb_233.json",
+  "https://www.kiwime.com/oqdb/files/2166587884/OpenQuizzDB_166/openquizzdb_166.json",
 ];
 
 var promises = [];
@@ -22,7 +25,8 @@ for (i = 0; i < quizzUrl.length; i++) {
       .get(quizzUrl[i])
       .then((response) => {
         quizzes.push(response.data);
-        // console.log(quizzes);
+        // console.log(quizzes[0].quizz.en[0].débutant);
+        //console.log(quizzes)
       })
       .catch((err) => {
         console.log("this is my error", err);
@@ -32,18 +36,39 @@ for (i = 0; i < quizzUrl.length; i++) {
 
 let newQuizz;
 
-Promise.all(promises)
-.then((resp) => {
-  quizzes.map((quizz) => {
-    newQuizz = 
-      {
-        supplier: quizz.fournisseur,
-        author: quizz.rédacteur,
-        title: quizz.thème,
-      }
-    
-    // console.log(newQuizz);
-  });
+Promise.all(promises).then((resp) => {
+  newQuizz = quizzes.map((quizz) => {
+    let englishQuizz = quizz.quizz.en[0].débutant;
+    // console.log("ENGLISH QUIZ", englishQuizz);
+    return {
+      supplier: quizz.fournisseur,
+      author: quizz.rédacteur,
+      title: quizz.thème,
+      quizzTotal: englishQuizz.map((question) => {
+        //   console.log("question", question)
+        return {
+          question: question.question,
+          propositions: question.propositions,
+          answer: question.réponse,
+          funFact:question.anecdote             
+          }
+        })
+    } 
+      })
+   
+      //   console.log("NEW QUIZ", newQuizz);
+ newQuizz.forEach((toto)=>{
+     QuizzModel.create(toto)
+    .then(dbRes => console.log(dbRes))
+    .catch(dbErr => console.error(dbErr))   
+    })
 });
 
+console.log("NEW QUIZ", newQuizz);
 
+
+// newQuizz.forEach((toto)=>{
+//  QuizzModel.create(toto)
+// .then(dbRes => console.log(dbRes))
+// .catch(dbErr => console.error(dbErr))   
+// })
--- models/Quizz.js
+++ models/Quizz.js
@@ -2,8 +2,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const quizzSchema = new Schema({
-
-})
+  supplier: String,
+  author: String,
+  image: String,
+  title: String,
+  quizzTotal: [
+    {
+   
+      question: String,
+      propositions: [String],
+      answer: String,
+      funFact: String,
+    },
+  ],
+  thema: {
+    type: String,
+    enum: [
+      "Nature",
+      "General Culture",
+      "Health and Beauty",
+      "Celebrity",
+      "Society",
+      "Miscellaneous",
+    ],
+  },
+  status:{
+      type:String,
+      enum:[
+          "Public",
+          "Private",
+      ]
+  }
+});
 
 const Quizz = mongoose.model("Quizz", quizzSchema);
 module.exports = Quizz;
+
+
+ //   index: { type: Number, index: true },
